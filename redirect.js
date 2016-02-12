f3425c052ca197d68228baea65fb85fd0dbbee94
MosesBones <deborahwirth@ip-144-38-118-58.dixie.edu>
2016-02-12 11:53:55 -0700

    onload

--- redirect.js
+++ redirect.js
@@ -1,4 +1,4 @@
-function myFunction() {
-	var host = window.location.host
+window.onload = function myFunction() {
+	var host = window.location.host 
 	var res = host.replace("dixie.life", "goo.gl");
 }
